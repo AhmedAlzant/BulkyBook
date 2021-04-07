b413128b6a4e0d3575bc1397a54d9c9be3bca4da
Ahmed <ahmad.alzant@gmail.com>
2021-04-07 19:43:15 +0400

    add CoverType to Repository adding update method to covertype repository

--- BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -18,6 +18,7 @@ namespace BulkyBook.DataAccess.Repository
             SP_Call = new SP_Call(_db);
         }
         public ICategoryRepository Category { get; private set; }
+        public ICoverTypeRepository CoverType { get; private set; }
         public ISP_Call SP_Call { get; private set; }
 
         public void Dispose()
